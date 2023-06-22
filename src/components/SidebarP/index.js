3a45db035766d4aa09b178507300fd122bcb7433
Vilmo18 <yvan.vilmorin@facsciences-uy1.cm>
2023-06-22 23:30:31 +0100

    clean

--- src/components/SidebarP/index.js
+++ src/components/SidebarP/index.js
@@ -9,51 +9,73 @@ import {
 } from 'cdbreact';
 import { NavLink } from 'react-router-dom';
 
-
-  export class Sidebar extends React.Component {
+export class Sidebar extends React.Component {
   render() {
-  return (
-    <div >
-          <div className="pt-5 "></div>
-   <div style={{ display: 'flex', height: '120vh', overflow: 'scroll initial' }} className='col-lg-3'>
-      <CDBSidebar textColor="#fff" backgroundColor="black">
-        <CDBSidebarHeader style={{backgroundColor:'orange' }} prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a className="text-decoration-none" style={{ color: 'inherit'}}>
-            Police
-          </a>
-        </CDBSidebarHeader>
+    return (
+      <div>
+        <div className="pt-5 "></div>
+        <div
+          style={{
+            display: 'flex',
+            height: '120vh',
+            overflow: 'scroll initial',
+          }}
+          className="col-lg-3"
+        >
+          <CDBSidebar textColor="#fff" backgroundColor="black">
+            <CDBSidebarHeader
+              style={{ backgroundColor: 'orange' }}
+              prefix={<i className="fa fa-bars fa-large"></i>}
+            >
+              <a className="text-decoration-none" style={{ color: 'inherit' }}>
+                Police
+              </a>
+            </CDBSidebarHeader>
 
-        <CDBSidebarContent className="sidebar-content">
-          <CDBSidebarMenu>
-          <NavLink exact to="/dashboard/police" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="home">Dashboard</CDBSidebarMenuItem>
-            </NavLink>
-          <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">My account</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/dashboard/crime" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="sticky-note">View all report</CDBSidebarMenuItem>
-            </NavLink>
-             
-            <NavLink exact to="/#" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="fas fa-sign-out-alt">Logout</CDBSidebarMenuItem>
-            </NavLink>
-          </CDBSidebarMenu>
-        </CDBSidebarContent>
+            <CDBSidebarContent className="sidebar-content">
+              <CDBSidebarMenu>
+                <NavLink
+                  exact
+                  to="/dashboard/police"
+                  activeClassName="activeClicked"
+                >
+                  <CDBSidebarMenuItem icon="home">Dashboard</CDBSidebarMenuItem>
+                </NavLink>
+                <NavLink exact to="/report" activeClassName="activeClicked">
+                  <CDBSidebarMenuItem icon="user">
+                    My account
+                  </CDBSidebarMenuItem>
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/dashboard/crime"
+                  activeClassName="activeClicked"
+                >
+                  <CDBSidebarMenuItem icon="sticky-note">
+                    View all report
+                  </CDBSidebarMenuItem>
+                </NavLink>
 
-        <CDBSidebarFooter style={{ textAlign: 'center' }} >
-          <div
-            style={{
-              padding: '20px 5px',
-            }}
-          >
-            Here to help victims
-          </div>
-        </CDBSidebarFooter>
-      </CDBSidebar>
-    </div>
-    </div>
-  );
+                <NavLink exact to="/#" activeClassName="activeClicked">
+                  <CDBSidebarMenuItem icon="fas fa-sign-out-alt">
+                    Logout
+                  </CDBSidebarMenuItem>
+                </NavLink>
+              </CDBSidebarMenu>
+            </CDBSidebarContent>
+
+            <CDBSidebarFooter style={{ textAlign: 'center' }}>
+              <div
+                style={{
+                  padding: '20px 5px',
+                }}
+              >
+                Here to help victims
+              </div>
+            </CDBSidebarFooter>
+          </CDBSidebar>
+        </div>
+      </div>
+    );
+  }
 }
-}
-
